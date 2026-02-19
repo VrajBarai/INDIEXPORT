d94240c7be1b134eb08e5651dd7f45ac721f54d5
VrajBarai <vrajbarai@gmail.com>
2026-02-20 04:02:54 +0530

    Connected frontend to backend URL

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,14 +1,13 @@
 import axios from "axios";
 
-const API = "/api/auth";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API = `${BASE_URL}/api/auth`;
 
 export const login = (email, password) =>
   axios.post(`${API}/login`, { email, password });
 
-export const register = (data) => {
-  console.log(data);
+export const register = (data) =>
   axios.post(`${API}/register`, data);
-}
 
 export const googleLogin = (token) =>
   axios.post(`${API}/google`, { token });
@@ -16,4 +15,4 @@ export const googleLogin = (token) =>
 export const logout = () => {
   localStorage.removeItem("token");
   localStorage.removeItem("role");
-}
+};
